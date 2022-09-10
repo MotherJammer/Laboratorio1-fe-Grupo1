62afc6e3c78ba5220375d973342315c0f6ceae8a
Matías Barolo <matias.barolo@usach.cl>
2022-09-10 18:31:27 -0300

    back conectado con front

--- pages/Emergency/addEmergency.vue
+++ pages/Emergency/addEmergency.vue
@@ -11,20 +11,25 @@
       <ul>
         <input type="text" id="emergency_status-id" v-model="emergency_status" placeholder="Reclutando">
       </ul>
+        <h4>Institucion</h4>
+        <ul>
+          <input type="int" id="id_in" v-model="id_in" placeholder="1">
+        </ul>
       <p>Detalles de la emergencia y requisitos para los voluntarios</p>
-      <textarea type="text" id="emergency_details-id" v-model="emergency_details" placeholder="Detalles" cols="100" rows="20"></textarea>
-      <div id="btn_upload">
+        <textarea type="text" id="emergency_details-id" v-model="emergency_details" placeholder="Detalles" cols="100" rows="20"></textarea>
+        <div id="btn_upload">
     <input type="submit" class="fadeIn fourth" value="Cargar Emergencia">
     </div>
       </form>
     </div>
-    
-    
-    
+
+
+
   </template>
 
 <script>
-    import { axios } from 'axios'
+
+    import axios from 'axios'
     export default {
       name: 'addEmergency',
       components: {
@@ -34,17 +39,21 @@
           emergency_name: '',
           emergency_status: '',
           emergency_details: '',
+          voluntarios: 0,
           error: false,
           error_msg: ''
         }
       },
       methods: {
         upload_Emergency () {
-          const data = 'http://localhost:8080/emergencia/'
+          const data = 'http://localhost:8081/emergencias/'
           axios.post(data, {
             nombre: this.emergency_name,
             estado_eme: this.emergency_status,
-            detalles: this.emergency_details
+            detalles: this.emergency_details,
+            voluntarios_reg: this.voluntarios,
+            id_in: this.id_in
+
           }).then(response => {
             console.log('creacion de emergencia exitosa')
           })
