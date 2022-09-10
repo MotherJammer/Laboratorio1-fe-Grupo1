f05408ab14e33d396e140275a911b687885f1a24
Matías Barolo <matias.barolo@usach.cl>
2022-09-10 19:53:20 -0300

    fix delete

--- pages/Emergency/deleteEmergency.vue
+++ pages/Emergency/deleteEmergency.vue
@@ -4,7 +4,7 @@
         <h4>Ingrese el ID de la emergencia a eliminar</h4>
         <form v-on:submit.prevent="delete_Emergency">
         <ul>
-        <input type="text" id="emergency-id" v-model="emergency_id" placeholder="Id de la emergencia">
+        <input type="text" id="emergency-id" v-model="id" placeholder="Id de la emergencia">
         </ul>
         <div id="btn_get">
         <input type="submit" class="fadeIn fourth" value="Eliminar Emergencia">
@@ -14,7 +14,7 @@
   </template>
 
 <script>
-    import { axios } from 'axios'
+    import axios  from 'axios'
     export default {
       name: 'deleteEmergency',
       components: {
@@ -30,12 +30,9 @@
       },
       methods: {
         delete_Emergency () {
-          const data = 'http://localhost:8080/emergencia/'
-          axios.delete(data, {
-            nombre: this.emergency_name,
-            estado_eme: this.emergency_status,
-            detalles: this.emergency_details
-          }).then(response => {
+          const id = this.id
+          const data = 'http://localhost:8081/emergencias/' + id
+          axios.delete(data).then(response => {
             console.log('eliminacion exitosa')
           })
         }
