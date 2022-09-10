e9717e1017544dce3f1281560be898ba6a19c8ee
Matías Barolo <matias.barolo@usach.cl>
2022-09-10 20:59:26 -0300

    conexion getEmergencias realizada

--- pages/Emergency/getEmergency.vue
+++ pages/Emergency/getEmergency.vue
@@ -2,42 +2,39 @@
     <div class="get_emergency">
         <h2> Obtener una emergencia de la base de datos</h2>
         <h4>Ingrese el ID de la emergencia a consultar</h4>
-        <form v-on:submit.prevent="change_Emergency">
+        <form v-on:submit.prevent="retrieveEmergencies">
         <ul>
-        <input type="text" id="emergency-id" v-model="emergency_id" placeholder="Id de la emergencia">
+        <input type="text" id="emergency-id" v-model="id" placeholder="Id de la emergencia">
         </ul>
         <div id="btn_get">
         <input type="submit" class="fadeIn fourth" value="Consultar Emergencia">
         </div>
+          <pre>{{emergencias}}</pre>
         </form>
     </div>
   </template>
 
 <script>
-    import { axios } from 'axios'
+    import axios from 'axios'
     export default {
       name: 'getEmergency',
       components: {
       },
       data: function () {
         return {
-          emergency_name: '',
-          emergency_status: '',
-          emergency_details: '',
-          error: false,
-          error_msg: ''
+          emergencias: []
         }
       },
       methods: {
-        get_Emergency () {
-          const data = 'http://localhost:8080/emergencia/'
-          axios.get(data, {
-            nombre: this.emergency_name,
-            estado_eme: this.emergency_status,
-            detalles: this.emergency_details
-          }).then(response => {
-            console.log('modificacion exitosa')
-          })
+        retrieveEmergencies () {
+          axios.get('http://localhost:8081/emergencias/' + this.id)
+            .then(response => {
+              console.log(response.data)
+              this.emergencias = response.data
+            })
+            .catch(error => {
+              console.log(error)
+            })
         }
       }
     }
