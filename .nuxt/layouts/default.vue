929740250f21dfb08d6ed16fc7687052b323439a
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2022-09-10 17:43:42 -0300

    Modificacion archivos nuxt

---
+++ .nuxt/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <nav><!-- Enlaces de navegación-->
+        <ul>
+          <li>
+            <a href="/Emergency/addEmergency">Agregar Emergencia</a>
+          </li>
+          <li>
+            <a href="/Emergency/modifyEmergency">Modificar Emergencia</a>
+          </li>
+          <li>
+            <a href="/Emergency/getEmergency">Consultar Emergencia</a>
+          </li>
+          <li>
+            <a href="/Emergency/deleteEmergency">Borrar Emergencia</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+      <Nuxt /><!-- Contenidos de las páginas-->
+    <footer><!-- contenido del footer -->
+    </footer>
+  </div>
+</template>
