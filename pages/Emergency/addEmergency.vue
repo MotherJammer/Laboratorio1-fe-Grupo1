3646684c6aac241574e298c5375d88c647ccf943
Matias Colil <matias.colil@usach.cl>
2022-09-18 05:39:30 -0300

    REST API addEmergency

--- pages/Emergency/addEmergency.vue
+++ pages/Emergency/addEmergency.vue
@@ -10,21 +10,12 @@
           <h5 class="p-2">Nombre emergencia</h5>
           <form class="p-2" v-on:submit.prevent="upload_Emergency">
             <ul>
-              <input
-                type="text"
-                id="emergency_name-id"
-                v-model="emergency_name"
-                placeholder="Terremoto en Talca"
-              />
+              <input type="text" id="emergency_name-id" v-model="emergency_name" placeholder="Terremoto en Talca" />
             </ul>
             <h5>Estado de la emergencia</h5>
             <ul>
-              <input
-                list="estadoEmergencia"
-                id="emergency_status-id"
-                v-model="emergency_status"
-                placeholder="Seleccione..."
-              />
+              <input list="estadoEmergencia" id="emergency_status-id" v-model="emergency_status"
+                placeholder="Seleccione..." />
               <datalist id="estadoEmergencia">
                 <option value="Reclutando"></option>
                 <option value="En proceso"></option>
@@ -38,41 +29,17 @@
             <!---------------- ACÁ ES DONDE SE CARGAN LAS HABILIDADES DE LA BASE --------------->
             <h5>Habilidades requeridas</h5>
             <div class="containerHabilidades">
-              <input type="checkbox" name="habilidad" value="1" />
-              Habilidad 1 <br />
-              <input type="checkbox" name="habilidad" value="2" />
-              Habilidad 2 <br />
-              <input type="checkbox" name="habilidad" value="3" />
-              Habilidad 3 <br />
-              <input type="checkbox" name="habilidad" value="4" />
-              Habilidad 4 <br />
-              <input type="checkbox" name="habilidad" value="5" />
-              Habilidad 5 <br />
-              <input type="checkbox" name="habilidad" value="6" />
-              Habilidad 6 <br />
-              <input type="checkbox" name="habilidad" value="7" />
-              Habilidad 7 <br />
-              <input type="checkbox" name="habilidad" value="8" />
-              Habilidad 8 <br />
-              <input type="checkbox" name="habilidad" value="9" />
-              Habilidad 9 <br />
-              <input type="checkbox" name="habilidad" value="10" />
-              Habilidad 10 <br />
+              <li v-for="habilidad in emergency_habilities">
+                <input type="checkbox"  v-model="emergency_sendHabilities" :value="habilidad.id" >
+                <label :for="habilidad.nombre"> {{habilidad.nombre}} </label>
+              </li>
             </div>
             <!----------------------------------------------------------------------------------->
             <h5>Detalles de la emergencia</h5>
-            <textarea
-              type="text"
-              id="emergency_details-id"
-              v-model="emergency_details"
-              placeholder="Detalles"
-              cols="50"
-              rows="10"
-            ></textarea>
+            <textarea type="text" id="emergency_details-id" v-model="emergency_details" placeholder="Detalles" cols="50"
+              rows="10"></textarea>
             <div id="btn_upload">
-              <b-button type="submit" class="fadeIn fourth"
-                >Cargar emergencia</b-button
-              >
+              <b-button type="submit" class="fadeIn fourth">Cargar emergencia</b-button>
             </div>
           </form>
         </div>
@@ -92,12 +59,13 @@ import axios from "axios";
 export default {
   name: "addEmergency",
   components: {},
-  data: function () {
+  data: () => {
     return {
       emergency_name: "",
       emergency_status: "",
       id_in: null,
-      emergency_habilities: [], //ACÁ ENTRARÍAN LAS HABILIDADES SELECCIONADAS
+      emergency_habilities: [],
+      emergency_sendHabilities :[], //ACÁ ENTRARÍAN LAS HABILIDADES SELECCIONADAS
       emergency_details: "",
       voluntarios: 0,
       error: false,
@@ -116,19 +84,29 @@ export default {
       ],
     };
   },
+  created() {
+    this.retrieveHabilidades()
+  }
+  ,
   methods: {
+    retrieveHabilidades() {
+      axios
+        .get("http://localhost:8081/habilidades")
+        .then((response) => {
+          console.log(response.data);
+          this.emergency_habilities = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+    },
+
     upload_Emergency() {
+      var dataFormat = new FormData();
+      dataFormat.append("idHabilidades",this.emergency_sendHabilities)
       const data = "http://localhost:8081/emergencias/";
-      //--------------------------------------------------------------------------------
-      //Mecanismo propuesto para recolectar las habilidades seleccionadas
-      var habilidades = document.getElementsByName("habilidad");
-      var habilidades_seleccionadas = [];
-      for (var i = 0; i < habilidades.length; i++) {
-        if (habilidades[i].checked) {
-          habilidades_seleccionadas.push(habilidades[i].value);
-        }
-      }
-      //--------------------------------------------------------------------------------
+      const dataHabilidadades = "http://localhost:8081/testHabilidades";
       axios
         .post(data, {
           nombre: this.emergency_name,
@@ -139,13 +117,13 @@ export default {
           //emergency_habilities: habilidades_seleccionadas, //ACÁ ENTRARÍAN LAS HABILIDADES SELECCIONADAS PARA AXIOS
         })
         .then((response) => {
-          //En esta alerta es donde se debe cargar el ID con que quedó la emergencia en la base
-          //Una vez se haya obtenido el servicio, se debe cambiar el mensaje del alert a el formato:
-          //alert("Emergencia cargada con éxito. El ID de la emergencia es: " + id_emergencia);
           alert("Emergencia cargada con éxito. El ID de la emergencia es: X");
           console.log("creacion de emergencia exitosa");
         });
+        axios
+        .post(dataHabilidadades, dataFormat)
     },
+
   },
 };
 </script>
